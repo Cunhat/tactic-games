7ebb5f1301cd97c4848049fa5d4b1239b52e7b4e
Tiago Cunha <tiago92cunha@gmail.com>
2024-01-05 00:32:32 +0000

    Add menu on mobile

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="flex  flex-col">
       <div className="h-[30%] w-full relative">
         <div className="bg-black/60 absolute top-0 left-0 right-0 bottom-0 z-10 h-full w-full flex justify-center items-center">
           <h1 className="text-7xl font bold">TACTIC GAMES</h1>
--- components/ui/nav-bar.tsx
+++ components/ui/nav-bar.tsx
@@ -6,9 +6,12 @@ import Image from "next/image";
 export const Navbar = () => {
   const [show, setShow] = React.useState(false);
   return (
-    <nav className="p-3 flex justify-between items-center sticky top-0 z-50 w-full bg-black">
+    <nav className="p-4 flex justify-between items-center sticky top-0 z-50 w-full bg-black">
       <Image src="/img/logo.png" width={60} height={60} alt="logo-img" />
-      <Icons.menu className="h-8 w-8" onClick={() => setShow(!show)} />
+      <Icons.menu
+        className="h-8 w-8 sm:hidden"
+        onClick={() => setShow(!show)}
+      />
       {show && (
         <div className="pt-[20%] p-4 absolute top-0 left-0 right-0 bottom-0 bg-black h-screen w-screen flex flex-col gap-[10%] items-center">
           <Image
@@ -23,14 +26,30 @@ export const Navbar = () => {
             className="h-8 w-8 absolute top-4 right-4"
             onClick={() => setShow(!show)}
           />
-          <ul className="flex flex-col gap-[5%] h-full">
-            <li className="text-3xl font-semibold text-center">Início</li>
-            <li className="text-3xl font-semibold text-center">Incrições</li>
-            <li className="text-3xl font-semibold text-center">Regulamento</li>
-            <li className="text-3xl font-semibold text-center">Galeria</li>
-          </ul>
+          <ListOfPages />
         </div>
       )}
+      <ListOfPages />
+      <div className="ml-auto"></div>
     </nav>
+  );
+};
+
+const ListOfPages = () => {
+  return (
+    <ul className="flex flex-col gap-[5%] h-full sm:flex-row sm:gap-6 sm:ml-auto">
+      <li className="text-3xl sm:text-lg font-semibold text-center hover:cursor-pointer hover:text-primary">
+        Início
+      </li>
+      <li className="text-3xl sm:text-lg font-semibold text-center hover:cursor-pointer hover:text-primary">
+        Incrições
+      </li>
+      <li className="text-3xl sm:text-lg font-semibold text-center hover:cursor-pointer hover:text-primary">
+        Regulamento
+      </li>
+      <li className="text-3xl sm:text-lg font-semibold text-center hover:cursor-pointer hover:text-primary">
+        Galeria
+      </li>
+    </ul>
   );
 };
