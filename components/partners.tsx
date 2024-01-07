67fac0b5e7f75985dd1c6e156fde8057b47564d0
Tiago Cunha <tiago92cunha@gmail.com>
2024-01-07 18:53:13 +0000

    Fix ts errors

--- components/partners.tsx
+++ components/partners.tsx
@@ -7,19 +7,20 @@ export const Partners = () => {
     function addAnimation() {
       scrollers.forEach((scroller) => {
         // add data-animated="true" to every `.scroller` on the page
-        scroller.setAttribute("data-animated", true);
+        scroller.setAttribute("data-animated", "true");
 
         // Make an array from the elements within `.scroller-inner`
         const scrollerInner = scroller.querySelector(".scroller__inner");
-        const scrollerContent = Array.from(scrollerInner.children);
+        const scrollerContent = Array.from(scrollerInner?.children!);
 
         // For each item in the array, clone it
         // add aria-hidden to it
         // add it into the `.scroller-inner`
         scrollerContent.forEach((item) => {
           const duplicatedItem = item.cloneNode(true);
+          // @ts-ignore
           duplicatedItem.setAttribute("aria-hidden", true);
-          scrollerInner.appendChild(duplicatedItem);
+          scrollerInner?.appendChild(duplicatedItem);
         });
       });
     }
